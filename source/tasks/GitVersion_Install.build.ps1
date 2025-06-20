eb4149de03af822d54ba82eca241ae9336f2dbbf
Dan Hughes <2237515+dan-hughes@users.noreply.github.com>
2025-06-20 10:41:20 +0100

    Allow GitVersion.Version to be overridden

--- source/tasks/GitVersion_Install.build.ps1
+++ source/tasks/GitVersion_Install.build.ps1
@@ -1,18 +1,38 @@
+<#
+    .SYNOPSIS
+        This is a build task that installs GitVersion.Tool using dotnet.
+
+    .PARAMETER BuildInfo
+        The build info object from ModuleBuilder. Defaults to an empty hashtable.
+
+    .NOTES
+        This is a build task that is primarily meant to be run by Invoke-Build but
+        wrapped by the Sampler project's build.ps1 (https://github.com/gaelcolas/Sampler).
+#>
+
 param
 (
+    # Build Configuration object
     [Parameter()]
-    [System.String]
-    $GitVersionVersion = (property GitVersionVersion '5.*')
+    [System.Collections.Hashtable]
+    $BuildInfo = (property BuildInfo @{ })
 )
 
 task GitVersion_Install {
     . Set-SamplerTaskVariable
 
+    $GitVersionVersion = '5.*'
+
+    if ($BuildInfo.GitVersion.Version)
+    {
+        $GitVersionVersion = $BuildInfo.GitVersion.Version
+    }
+
     try
     {
         Write-Build DarkGray "Installing GitVersion.Tool version '$GitVersionVersion'"
 
-        dotnet tool install --global GitVersion.Tool --version $GitVersionVersion
+        Start-Process dotnet -ArgumentList "tool install --global GitVersion.Tool --version $GitVersionVersion" -Wait
     }
     catch
     {
